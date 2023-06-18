fdd1fc76302007ef8f7a1adca6af8784487dd077
haewon999 <1000hae1@naver.com>
2023-06-19 08:30:41 +0900

    6월19일

---
+++ src/algorithm/section09/DynamicProgramming.java
@@ -0,0 +1,14 @@
+package algorithm.section09;
+
+import java.util.Scanner;
+
+//동적계획법 dp 중요
+//간단한 문제를 분리하여 부분의 문제들을 해결, 최종적으로 복잡한 문제의 답을 구하는 방법
+
+
+class DynamicProgramming{
+	public static void maina(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
